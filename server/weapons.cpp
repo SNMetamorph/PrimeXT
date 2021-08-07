19ecf7274ec800f1c7b4dc6ae9966999cba4cedb
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2021-08-07 18:44:44 +0400

    server: fixed crash when using impulse 101

--- server/weapons.cpp
+++ server/weapons.cpp
@@ -1098,7 +1098,7 @@ void CBasePlayerAmmo :: DefaultTouch( CBaseEntity *pOther )
 //=========================================================
 int CBasePlayerWeapon::ExtractAmmo( CBasePlayerWeapon *pWeapon )
 {
-	int			iReturn;
+	int	iReturn = 0;
 
 	if ( pszAmmo1() != NULL )
 	{
