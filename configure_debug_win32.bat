9cad0d2853a8a11b3f5811318278ce3176617142
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2021-08-15 12:37:52 +0400

    waf: added configure example scripts for Windows

---
+++ configure_debug_win32.bat
@@ -0,0 +1,2 @@
+@echo off
+waf configure -T debug --prefix=..\xash3d-fwgs\build-x86 --enable-physx
---
+++ configure_release_win32.bat
@@ -0,0 +1,2 @@
+@echo off
+waf configure -T release --prefix=..\xash3d-fwgs\build-x86 --enable-physx
