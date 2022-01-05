1b5795d26d1ade4c29c90047a85ad4d34ebadcd7
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-01-06 03:29:02 +0400

    ci: removed Waf build scripts

--- configure_debug_win32.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-waf configure -T debug --prefix=..\xash3d-fwgs\build-x86 --enable-physx
--- configure_release_win32.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-waf configure -T release --prefix=..\xash3d-fwgs\build-x86 --enable-physx
