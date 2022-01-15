f65707ee252cbe420f0961762d8c32d61aafbd50
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-01-16 02:12:02 +0400

    documentation: fixed text encoding and links

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -23,14 +23,11 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/SNMetamorph/PrimeXT/tree/master/documentation',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/SNMetamorph/PrimeXT/tree/master/documentation',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
