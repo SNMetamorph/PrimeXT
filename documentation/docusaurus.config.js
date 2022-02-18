0fd2c06b52908f59223eb0f23cfcd26929b9fb9d
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-02-19 02:58:00 +0400

    documentation: added post to blog about Oct 2021 alpha release

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -54,7 +54,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/SNMetamorph/PrimeXT',
             label: 'GitHub',
             position: 'right',
           },
