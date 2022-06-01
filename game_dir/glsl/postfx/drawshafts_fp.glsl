8c1f05630c35896ce387323b9e3e52e3dbc4439c
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-06-01 22:34:12 +0400

    game_dir: glsl: minor refactoring of sunshafts shader code

--- game_dir/glsl/postfx/drawshafts_fp.glsl
+++ game_dir/glsl/postfx/drawshafts_fp.glsl
@@ -39,22 +39,24 @@ void main( void )
 	float sunDist = saturate( u_LightOrigin.z ) * saturate( 1.0 - saturate( length( sunVec ) * 0.2 ));
 	sunVec *= 0.1 * u_LightOrigin.z;
 
-	vec4 accum = texture2D( u_ColorMap, tc );
-	float fShaftsMask = saturate( 1.00001 - accum.w );
-	float fBlend = accum.w * 0.85;	// shaft brightness
+	vec4 colorMapSample = texture2D( u_ColorMap, tc );
+	float fShaftsMask = saturate( 1.0001 - colorMapSample.w );
+	float fBlend = colorMapSample.w * 0.85;	// shaft brightness
 	float weight_sum = 1.0;
+	vec3 accum = colorMapSample.rgb;
 
-	for (float i = 1.0; i < 32.0; i++) {
+	for (float i = 1.0; i < 32.0; i++) 
+	{
 		vec2 offset = sunVec.xy * 0.25 * i;
 		float weight = 1.0 / max(length(offset), 0.0001);
-		accum += texture2D( u_ColorMap, tc + offset ).rgba * weight;
+		accum += texture2D( u_ColorMap, tc + offset ).rgb * weight;
 		weight_sum += weight;	
 	}
 
 	accum /= weight_sum;
- 	accum *= 2.0 * vec4( sunDist, sunDist, sunDist, 1.0 );
+ 	accum *= 2.0 * sunDist;
 	
- 	vec3 cScreen = texture2D( u_ScreenMap, tc ).rgb;      
+ 	vec3 cScreen = texture2D( u_ScreenMap, tc ).rgb;
 	vec3 outColor = cScreen + accum.rgb * fBlend * sunColor * (1.0 - cScreen);
 	outColor = BlendSoftLight( outColor, sunColor * fShaftsMask * 0.5 + 0.5 );
 	gl_FragColor = vec4( outColor, 1.0 );
