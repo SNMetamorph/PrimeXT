996c27cb2c0cfed892a3b6ded289203b862dffd6
ncuxonaT <ncuxonat@mail.ru>
2022-10-30 21:55:38 +0300

    Studiomodel TBN fix
    1. Correct TBN for mirrored UVs
    2. Always smooth coincident vertices TBN.

--- client/render/gl_studio_init.cpp
+++ client/render/gl_studio_init.cpp
@@ -1490,28 +1490,34 @@ void CStudioModelRenderer :: SetupSubmodelVerts( const mstudiomodel_t *pSubModel
 				tVect += triTVect[vertToTriMap[vertID][triID]];
 			}
 
-			if( smooth_tbn )
+			// smooth TBN
+			Vector vertPos1 = m_arrayverts[vertID]; // transformed to global pose to avoid seams
+			Vector4D vertUV1 = m_arrayxvert[vertID].stcoord;
+			Vector vertNorm1 = m_arrayxvert[vertID].normal;
+
+			for( int vertID2 = 0; vertID2 < m_nNumArrayVerts; vertID2++ )
 			{
-				// smooth TBN
-				Vector vertPos1 = m_arrayverts[vertID]; // transformed to global pose to avoid seams
-
-				for( int vertID2 = 0; vertID2 < m_nNumArrayVerts; vertID2++ )
+				if( vertID2 == vertID )
+					continue;
+				
+				Vector vertPos2 = m_arrayverts[vertID2]; // transformed to global pose to avoid seams
+				if( vertPos1 != vertPos2 )
+					continue;
+				
+				Vector4D vertUV2 = m_arrayxvert[vertID2].stcoord;
+				if (vertUV1 != vertUV2)
+					continue;
+				
+				Vector vertNorm2 = m_arrayxvert[vertID2].normal;
+				if (vertNorm1 != vertNorm2)
+					continue;	
+					
+				for( int triID2 = 0; triID2 < vertToTriMap[vertID2].Size(); triID2++ )
 				{
-					if( vertID2 == vertID )
-						continue;
-
-					Vector vertPos2 = m_arrayverts[vertID2]; // transformed to global pose to avoid seams
-
-					if( vertPos1 == vertPos2 )
-					{
-						for( int triID2 = 0; triID2 < vertToTriMap[vertID2].Size(); triID2++ )
-						{
-							sVect += triSVect[vertToTriMap[vertID2][triID2]];
-							tVect += triTVect[vertToTriMap[vertID2][triID2]];
-						}
-					}
-				}
-			}
+					sVect += triSVect[vertToTriMap[vertID2][triID2]];
+					tVect += triTVect[vertToTriMap[vertID2][triID2]];
+				}				
+			}			
 
 			// rotate tangent and binormal back to bone space
 			ComputeSkinMatrix( &m_arrayxvert[vertID], bones, skinMat );
@@ -1519,23 +1525,11 @@ void CStudioModelRenderer :: SetupSubmodelVerts( const mstudiomodel_t *pSubModel
 			sVect = skinMat.VectorIRotate( sVect );
 			tVect = skinMat.VectorIRotate( tVect );
 
-			if( !smooth_tbn )
-			{
-				Vector tmpVect = CrossProduct( sVect, tVect );
-				bool leftHanded = DotProduct( tmpVect, normal ) < 0.0f;
-
-				if( !leftHanded )
-				{
-					tVect = CrossProduct( normal, sVect );
-					sVect = CrossProduct( tVect, normal );
-				}
-				else
-				{
-					// make studiomodel TBN right-side coordinate system for accordance with TBN of bmodels
-					tVect = CrossProduct( sVect, normal );
-					sVect = CrossProduct( tVect, normal );
-				}
-			}
+			//orthogonalization
+			sVect = sVect.Normalize();
+			tVect = tVect.Normalize();			
+			sVect = sVect - normal * DotProduct(normal, sVect);
+			tVect = CrossProduct(normal, sVect) * Q_sign(DotProduct(CrossProduct(normal, sVect) , tVect));	
 
 			finalSVect = sVect.Normalize();
 			finalTVect = tVect.Normalize();
