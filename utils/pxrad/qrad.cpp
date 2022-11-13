5ffb3437786d4b3f1473aadfe11085b73fe58c86
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-11-13 14:14:37 +0400

    utils: pxrad: enabled printing detailed build info to log

--- utils/pxrad/qrad.cpp
+++ utils/pxrad/qrad.cpp
@@ -13,6 +13,7 @@
 #include "qrad.h"
 #include "app_info.h"
 #include "crashhandler.h"
+#include "build_info.h"
 #include <mimalloc-override.h>
 
 /*
@@ -2669,7 +2670,12 @@ int main( int argc, char **argv )
 
 	Sys_InitLogAppend( va( "%s.log", source ));
 
-	Msg( "\n%s %s (%s)\n", TOOLNAME, VERSIONSTRING, __DATE__ );
+	Msg( "\n%s %s (%s, commit %s, arch %s, platform %s)\n", TOOLNAME, VERSIONSTRING, 
+		BuildInfo::GetDate(), 
+		BuildInfo::GetCommitHash(), 
+		BuildInfo::GetArchitecture(), 
+		BuildInfo::GetPlatform()
+	);
 
 	PrintRadSettings();
 
