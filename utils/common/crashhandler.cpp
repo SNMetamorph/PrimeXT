20a1d960fdfd1a46a407f3cfc047b524ef88da79
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2022-11-29 11:35:21 +0400

    utils: common: crashhandler: fixed build errors on ARM architecture

--- utils/common/crashhandler.cpp
+++ utils/common/crashhandler.cpp
@@ -365,12 +365,12 @@ static void Sys_Crash( int signal, siginfo_t *si, void *context)
 	#endif
 #elif XASH_ARM && XASH_64BIT
 	pc = (void*)ucontext->uc_mcontext.pc;
-	bp = (void*)ucontext->uc_mcontext.regs[29];
-	sp = (void*)ucontext->uc_mcontext.sp;
+	bp = (void**)ucontext->uc_mcontext.regs[29];
+	sp = (void**)ucontext->uc_mcontext.sp;
 #elif XASH_ARM
 	pc = (void*)ucontext->uc_mcontext.arm_pc;
-	bp = (void*)ucontext->uc_mcontext.arm_fp;
-	sp = (void*)ucontext->uc_mcontext.arm_sp;
+	bp = (void**)ucontext->uc_mcontext.arm_fp;
+	sp = (void**)ucontext->uc_mcontext.arm_sp;
 #endif
 
 #if 0
