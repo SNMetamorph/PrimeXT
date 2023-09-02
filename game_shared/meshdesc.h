7be121e3226710a0af607137398b71dbd273b7c9
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2023-09-02 20:48:05 +0400

    game_shared: meshdesc: minor code refactoring

--- game_shared/meshdesc.h
+++ game_shared/meshdesc.h
@@ -18,6 +18,7 @@ GNU General Public License for more details.
 
 #include "studio.h"
 #include "areanode.h"
+#include <stdint.h>
 
 #define MAX_AREA_DEPTH	5
 #define MAX_AREANODES	BIT( MAX_AREA_DEPTH + 1 )
@@ -48,94 +49,95 @@ typedef struct mfacet_s
 	Vector		mins, maxs;		// an individual size of each facet
 	vec3_t		edge1, edge2;	// new trace stuff
 	byte		numplanes;		// because numplanes for each facet can't exceeds MAX_FACET_PLANES!
-	uint		*indices;		// a indexes into mesh plane pool
+	uint32_t	*indices;		// a indexes into mesh plane pool
 } mfacet_t;
 
 typedef struct
 {
 	Vector		mins, maxs;
-	int		numfacets;
-	int		numplanes;
-	mfacet_t		*facets;
-	mplane_t		*planes;			// shared plane pool
+	int			numfacets;
+	int			numplanes;
+	mfacet_t	*facets;
+	mplane_t	*planes;		// shared plane pool
 } mmesh_t;
 
 class CMeshDesc
 {
+public:
+	CMeshDesc();
+	~CMeshDesc();
+
+	// get cached collision
+	mmesh_t *GetMesh() { return (m_bMeshBuilt) ? &m_mesh : NULL; }
+	areanode_t *GetHeadNode() { return (has_tree) ? areanodes : NULL; }
+
+	void SetDebugName(const char *name) { m_debugName = name; }
+	void SetModel(const model_t *mod) { m_pModel = (model_t *)mod; }
+	void PrintMeshInfo();
+	bool StudioConstructMesh();
+	void FreeMesh();
+
+	// mesh construction
+	bool InitMeshBuild(int numTrinagles);
+	bool FinishMeshBuild();
+	// accepts triangles in model local space
+	bool AddMeshTrinagle(const mvert_t triangle[3], int skinref);
+	bool AddMeshTrinagle(const Vector triangle[3]);
+
 private:
+	void FreeMeshBuild();
+
+	// pacifier stuff
+	void StartPacifier();
+	void UpdatePacifier(float percent);
+	void EndPacifier(float total);
+
+	// studio models processing
+	void ExtractAnimValue(int frame, mstudioanim_t *panim, int dof, float scale, float &v1);
+	void StudioCalcBoneTransform(int frame, mstudiobone_t *pbone, mstudioanim_t *panim, Vector &pos, Vector4D &q);
+	bool StudioLoadCache(const char *pCacheName);
+	bool StudioSaveCache(const char *pCacheName);
+
+	// linked list operations
+	void InsertLinkBefore(link_t *l, link_t *before);
+	void RemoveLink(link_t *l);
+	void ClearLink(link_t *l);
+
+	// AABB tree contsruction
+	areanode_t *CreateAreaNode(int depth, const Vector &mins, const Vector &maxs);
+	void RelinkFacet(mfacet_t *facet);
+
+	// plane cache
+	int PlaneFromPoints(const Vector &p0, const Vector &p1, const Vector &p2);
+	int FindFloatPlane(const Vector &normal, float dist);
+	int CreateNewFloatPlane(const Vector &srcnormal, float dist, int hash);
+	bool PlaneEqual(const mplane_t *p, const Vector &normal, float dist);
+	int PlaneTypeForNormal(const Vector &normal);
+	int SnapNormal(Vector &normal);
+
 	mmesh_t		m_mesh;
 	const char	*m_debugName;	// just for debug purpoces
 	areanode_t	areanodes[MAX_AREANODES];	// AABB tree for speedup trace test
-	int		numareanodes;
-	bool	has_tree;			// build AABB tree
-	bool	m_bMeshBuilt;		// indicates is mesh ready for use
-	int		m_iTotalPlanes;		// just for stats
-	int		m_iAllocPlanes;		// allocated count of planes
-	int		m_iHashPlanes;		// total count of hashplanes
-	int		m_iNumTris;			// if > 0 we are in build mode
-	size_t	mesh_size;			// mesh total size
-	model_t	*m_pModel;			// parent model pointer
+	int			numareanodes;
+	bool		has_tree;			// build AABB tree
+	bool		m_bMeshBuilt;		// indicates is mesh ready for use
+	int			m_iTotalPlanes;		// just for stats
+	int			m_iAllocPlanes;		// allocated count of planes
+	int			m_iHashPlanes;		// total count of hashplanes
+	int			m_iNumTris;			// if > 0 we are in build mode
+	size_t		mesh_size;			// mesh total size
+	model_t		*m_pModel;			// parent model pointer
 
 	// used only while mesh is constructing
 	mfacet_t	*m_srcFacets;
 	hashplane_t	**m_srcPlaneHash;
 	hashplane_t	*m_srcPlanePool;
-	uint		*m_srcPlaneElems;
-	uint		*m_curPlaneElems; // sliding pointer
+	uint32_t	*m_srcPlaneElems;
+	uint32_t	*m_curPlaneElems; // sliding pointer
 
 	// pacifier stuff
 	bool		m_bShowPacifier;
 	int			m_iOldPercent;
-public:
-	CMeshDesc();
-	~CMeshDesc();
-
-	// mesh construction
-	bool InitMeshBuild( int numTrinagles ); 
-	bool FinishMeshBuild( void );
-	void FreeMeshBuild( void );
-	void FreeMesh( void );
-
-	// accepts triangles in model local space
-	bool AddMeshTrinagle(const mvert_t triangle[3], int skinref);
-	bool AddMeshTrinagle(const Vector triangle[3]);
-
-	void SetDebugName( const char *name ) { m_debugName = name; }
-	void SetModel( const model_t *mod ) { m_pModel = (model_t *)mod; }
-	void PrintMeshInfo( void );
-
-	// pacifier stuff
-	void StartPacifier( void );
-	void UpdatePacifier( float percent );
-	void EndPacifier( float total );
-
-	// studio models processing
-	void ExtractAnimValue( int frame, mstudioanim_t *panim, int dof, float scale, float &v1 );
-	void StudioCalcBoneTransform( int frame, mstudiobone_t *pbone, mstudioanim_t *panim, Vector &pos, Vector4D &q );
-	bool StudioLoadCache( const char *pCacheName );
-	bool StudioSaveCache( const char *pCacheName );
-	bool StudioConstructMesh( void );
-
-	// linked list operations
-	void InsertLinkBefore( link_t *l, link_t *before );
-	void RemoveLink( link_t *l );
-	void ClearLink( link_t *l );
-
-	// AABB tree contsruction
-	areanode_t *CreateAreaNode( int depth, const Vector &mins, const Vector &maxs );
-	void RelinkFacet( mfacet_t *facet );
-	areanode_t *GetHeadNode( void ) { return (has_tree) ? areanodes : NULL; }
-
-	// plane cache
-	int PlaneFromPoints( const Vector &p0, const Vector &p1, const Vector &p2 );
-	int FindFloatPlane( const Vector &normal, float dist );
-	int CreateNewFloatPlane( const Vector &srcnormal, float dist, int hash );
-	bool PlaneEqual( const mplane_t *p, const Vector &normal, float dist );
-	int PlaneTypeForNormal( const Vector &normal );
-	int SnapNormal( Vector &normal );
-
-	// get cached collision
-	mmesh_t *GetMesh() { return (m_bMeshBuilt) ? &m_mesh : NULL; }
 };
 
 CMeshDesc *UTIL_GetCollisionMesh( int modelindex );
