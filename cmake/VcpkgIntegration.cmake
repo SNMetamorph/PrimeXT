042e57081c2460601f32fdfcdd481d632cfcc0b8
Bohdan Shulyar <mars873@gmail.com>
2023-10-31 17:30:06 +0400

    cmake: fixed build errors on Android platform

--- cmake/VcpkgIntegration.cmake
+++ cmake/VcpkgIntegration.cmake
@@ -1,12 +1,14 @@
 # allows to transparently pass in custom toolchain file along with vcpkg toolchain
 # include this module in main CMakeLists.txt script BEFORE project() directive
-if(CMAKE_TOOLCHAIN_FILE) 
-	if (NOT CMAKE_TOOLCHAIN_FILE MATCHES "vcpkg.cmake$")
-		set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_TOOLCHAIN_FILE}")
+if(NOT VCPKG_TARGET_ANDROID)
+	if(CMAKE_TOOLCHAIN_FILE) 
+		if (NOT CMAKE_TOOLCHAIN_FILE MATCHES "vcpkg.cmake$")
+			set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_TOOLCHAIN_FILE}")
+			set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/external/vcpkg/scripts/buildsystems/vcpkg.cmake")
+		endif()
+	else()
 		set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/external/vcpkg/scripts/buildsystems/vcpkg.cmake")
 	endif()
-else()
-	set(CMAKE_TOOLCHAIN_FILE "${CMAKE_SOURCE_DIR}/external/vcpkg/scripts/buildsystems/vcpkg.cmake")
 endif()
 
 set(VCPKG_OVERLAY_PORTS "${CMAKE_SOURCE_DIR}/vcpkg_ports")
