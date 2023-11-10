1952170a5d5aaa28a5e71d6719805344b3a5e045
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2023-11-10 16:52:09 +0400

    server: restructurized glock weapon source code

---
+++ server/entities/ammo_9mmclip.cpp
@@ -0,0 +1,42 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+
+#include "ammo_9mmclip.h"
+
+LINK_ENTITY_TO_CLASS( ammo_glockclip, CGlockAmmo );
+LINK_ENTITY_TO_CLASS( ammo_9mmclip, CGlockAmmo );
+
+void CGlockAmmo::Spawn()
+{ 
+	Precache( );
+	SET_MODEL(ENT(pev), "models/w_9mmclip.mdl");
+	CBasePlayerAmmo::Spawn( );
+}
+
+void CGlockAmmo::Precache()
+{
+	PRECACHE_MODEL ("models/w_9mmclip.mdl");
+	PRECACHE_SOUND("items/9mmclip1.wav");
+}
+
+BOOL CGlockAmmo::AddAmmo( CBaseEntity *pOther ) 
+{ 
+	if (pOther->GiveAmmo( AMMO_GLOCKCLIP_GIVE, "9mm", _9MM_MAX_CARRY ) != -1)
+	{
+		EMIT_SOUND(ENT(pev), CHAN_ITEM, "items/9mmclip1.wav", 1, ATTN_NORM);
+		return TRUE;
+	}
+	return FALSE;
+}
