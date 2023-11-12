392786e4c5db59f14dff7bf804f3d8018b102beb
git <APAmk2@users.noreply.github.com>
2023-11-12 12:50:54 +0400

    Вынес item_security из items.cpp

---
+++ server/items/securitycard.cpp
@@ -0,0 +1,33 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   Use, distribution, and modification of this source code and/or resulting
+*   object code is restricted to non-commercial enhancements to products from
+*   Valve LLC.  All other use, distribution, or modification is prohibited
+*   without written permission from Valve LLC.
+*
+****/
+#include "securitycard.h"
+
+CItemSecurity::Spawn( void )
+{ 
+	Precache( );
+	SET_MODEL(ENT(pev), "models/w_security.mdl");
+	CItem::Spawn( );
+}
+
+CItemSecurity::Precache( void )
+{
+	PRECACHE_MODEL ("models/w_security.mdl");
+}
+	
+CItemSecurity::MyTouch( CBasePlayer *pPlayer )
+{
+	pPlayer->m_rgItems[ITEM_SECURITY] += 1;
+	return TRUE;
+}
