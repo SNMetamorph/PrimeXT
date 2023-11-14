c46ddecc3ee2486da4f6d4b50c2d05a5d799ec8e
git <APAmk2@users.noreply.github.com>
2023-11-14 21:09:57 +0400

    server: restructurized monster_zombie source code

--- server/monsters/zombie.cpp
+++ server/monsters/zombie.cpp
@@ -16,96 +16,9 @@
 // Zombie
 //=========================================================
 
-// UNDONE: Don't flinch every time you get hit
-
-#include	"extdll.h"
-#include	"util.h"
-#include	"cbase.h"
-#include	"monsters.h"
-#include	"schedule.h"
-
-
-//=========================================================
-// Monster's Anim Events Go Here
-//=========================================================
-#define	ZOMBIE_AE_ATTACK_RIGHT		0x01
-#define	ZOMBIE_AE_ATTACK_LEFT		0x02
-#define	ZOMBIE_AE_ATTACK_BOTH		0x03
-
-#define ZOMBIE_FLINCH_DELAY			2		// at most one flinch every n secs
-
-class CZombie : public CBaseMonster
-{
-	DECLARE_CLASS( CZombie, CBaseMonster );
-public:
-	void Spawn( void );
-	void Precache( void );
-	void SetYawSpeed( void );
-	int  Classify ( void );
-	void HandleAnimEvent( MonsterEvent_t *pEvent );
-	int IgnoreConditions ( void );
-
-	float m_flNextFlinch;
-
-	void PainSound( void );
-	void AlertSound( void );
-	void IdleSound( void );
-	void AttackSound( void );
-
-	static const char *pAttackSounds[];
-	static const char *pIdleSounds[];
-	static const char *pAlertSounds[];
-	static const char *pPainSounds[];
-	static const char *pAttackHitSounds[];
-	static const char *pAttackMissSounds[];
-
-	// No range attacks
-	BOOL CheckRangeAttack1 ( float flDot, float flDist ) { return FALSE; }
-	BOOL CheckRangeAttack2 ( float flDot, float flDist ) { return FALSE; }
-	int TakeDamage( entvars_t *pevInflictor, entvars_t *pevAttacker, float flDamage, int bitsDamageType );
-};
+#include	"zombie.h"
 
 LINK_ENTITY_TO_CLASS( monster_zombie, CZombie );
-
-const char *CZombie::pAttackHitSounds[] = 
-{
-	"zombie/claw_strike1.wav",
-	"zombie/claw_strike2.wav",
-	"zombie/claw_strike3.wav",
-};
-
-const char *CZombie::pAttackMissSounds[] = 
-{
-	"zombie/claw_miss1.wav",
-	"zombie/claw_miss2.wav",
-};
-
-const char *CZombie::pAttackSounds[] = 
-{
-	"zombie/zo_attack1.wav",
-	"zombie/zo_attack2.wav",
-};
-
-const char *CZombie::pIdleSounds[] = 
-{
-	"zombie/zo_idle1.wav",
-	"zombie/zo_idle2.wav",
-	"zombie/zo_idle3.wav",
-	"zombie/zo_idle4.wav",
-};
-
-const char *CZombie::pAlertSounds[] = 
-{
-	"zombie/zo_alert10.wav",
-	"zombie/zo_alert20.wav",
-	"zombie/zo_alert30.wav",
-};
-
-const char *CZombie::pPainSounds[] = 
-{
-	"zombie/zo_pain1.wav",
-	"zombie/zo_pain2.wav",
-};
 
 //=========================================================
 // Classify - indicates this monster's place in the 
