4c399c80261d0fb265e8777d1be9fe059ff1b97c
git <APAmk2@users.noreply.github.com>
2023-11-14 21:09:57 +0400

    server: restructurized monster_zombie source code

---
+++ server/monsters/zombie.h
@@ -0,0 +1,107 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+//=========================================================
+// Zombie
+//=========================================================
+
+// UNDONE: Don't flinch every time you get hit
+
+#pragma once
+
+#include	"extdll.h"
+#include	"util.h"
+#include	"cbase.h"
+#include	"monsters.h"
+#include	"schedule.h"
+
+//=========================================================
+// Monster's Anim Events Go Here
+//=========================================================
+#define	ZOMBIE_AE_ATTACK_RIGHT		0x01
+#define	ZOMBIE_AE_ATTACK_LEFT		0x02
+#define	ZOMBIE_AE_ATTACK_BOTH		0x03
+
+#define ZOMBIE_FLINCH_DELAY			2		// at most one flinch every n secs
+
+class CZombie : public CBaseMonster
+{
+	DECLARE_CLASS( CZombie, CBaseMonster );
+public:
+	void Spawn( void );
+	void Precache( void );
+	void SetYawSpeed( void );
+	int  Classify ( void );
+	void HandleAnimEvent( MonsterEvent_t *pEvent );
+	int IgnoreConditions ( void );
+
+	float m_flNextFlinch;
+
+	void PainSound( void );
+	void AlertSound( void );
+	void IdleSound( void );
+	void AttackSound( void );
+
+	static const char *pAttackSounds[];
+	static const char *pIdleSounds[];
+	static const char *pAlertSounds[];
+	static const char *pPainSounds[];
+	static const char *pAttackHitSounds[];
+	static const char *pAttackMissSounds[];
+
+	// No range attacks
+	BOOL CheckRangeAttack1 ( float flDot, float flDist ) { return FALSE; }
+	BOOL CheckRangeAttack2 ( float flDot, float flDist ) { return FALSE; }
+	int TakeDamage( entvars_t *pevInflictor, entvars_t *pevAttacker, float flDamage, int bitsDamageType );
+};
+
+const char *CZombie::pAttackHitSounds[] = 
+{
+	"zombie/claw_strike1.wav",
+	"zombie/claw_strike2.wav",
+	"zombie/claw_strike3.wav",
+};
+
+const char *CZombie::pAttackMissSounds[] = 
+{
+	"zombie/claw_miss1.wav",
+	"zombie/claw_miss2.wav",
+};
+
+const char *CZombie::pAttackSounds[] = 
+{
+	"zombie/zo_attack1.wav",
+	"zombie/zo_attack2.wav",
+};
+
+const char *CZombie::pIdleSounds[] = 
+{
+	"zombie/zo_idle1.wav",
+	"zombie/zo_idle2.wav",
+	"zombie/zo_idle3.wav",
+	"zombie/zo_idle4.wav",
+};
+
+const char *CZombie::pAlertSounds[] = 
+{
+	"zombie/zo_alert10.wav",
+	"zombie/zo_alert20.wav",
+	"zombie/zo_alert30.wav",
+};
+
+const char *CZombie::pPainSounds[] = 
+{
+	"zombie/zo_pain1.wav",
+	"zombie/zo_pain2.wav",
+};
