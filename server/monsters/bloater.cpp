eab8afb01f985f9e655e6ed780eaf022beb6e8ad
git <APAmk2@users.noreply.github.com>
2023-11-16 21:38:26 +0400

    server: restructurized bloater NPC source code

--- server/monsters/bloater.cpp
+++ server/monsters/bloater.cpp
@@ -16,39 +16,7 @@
 // Bloater
 //=========================================================
 
-#include	"extdll.h"
-#include	"util.h"
-#include	"cbase.h"
-#include	"monsters.h"
-#include	"schedule.h"
-
-
-//=========================================================
-// Monster's Anim Events Go Here
-//=========================================================
-#define	BLOATER_AE_ATTACK_MELEE1		0x01
-
-
-class CBloater : public CBaseMonster
-{
-	DECLARE_CLASS( CBloater, CBaseMonster );
-public:
-	void Spawn( void );
-	void Precache( void );
-	void SetYawSpeed( void );
-	int  Classify ( void );
-	void HandleAnimEvent( MonsterEvent_t *pEvent );
-
-	void PainSound( void );
-	void AlertSound( void );
-	void IdleSound( void );
-	void AttackSnd( void );
-
-	// No range attacks
-	BOOL CheckRangeAttack1 ( float flDot, float flDist ) { return FALSE; }
-	BOOL CheckRangeAttack2 ( float flDot, float flDist ) { return FALSE; }
-	int TakeDamage( entvars_t *pevInflictor, entvars_t *pevAttacker, float flDamage, int bitsDamageType );
-};
+#include	"bloater.h"
 
 LINK_ENTITY_TO_CLASS( monster_bloater, CBloater );
 
