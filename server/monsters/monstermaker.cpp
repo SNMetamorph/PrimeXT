700fdac94137eb1ec59b90e5f1f55067cb19ed18
git <APAmk2@users.noreply.github.com>
2023-11-16 14:45:09 +0400

    server: restructurized monstermaker source code

--- server/monsters/monstermaker.cpp
+++ server/monsters/monstermaker.cpp
@@ -17,44 +17,7 @@
 // in the game.
 //=========================================================
 
-#include "extdll.h"
-#include "util.h"
-#include "cbase.h"
-#include "monsters.h"
-#include "saverestore.h"
-
-// Monstermaker spawnflags
-#define	SF_MONSTERMAKER_START_ON	1 // start active ( if has targetname )
-#define	SF_MONSTERMAKER_CYCLIC		4 // drop one monster every time fired.
-#define SF_MONSTERMAKER_MONSTERCLIP	8 // Children are blocked by monsterclip
-
-//=========================================================
-// MonsterMaker - this ent creates monsters during the game.
-//=========================================================
-class CMonsterMaker : public CBaseMonster
-{
-	DECLARE_CLASS( CMonsterMaker, CBaseMonster );
-public:
-	void Spawn( void );
-	void Precache( void );
-	void KeyValue( KeyValueData* pkvd);
-	void ToggleUse ( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
-	void CyclicUse ( CBaseEntity *pActivator, CBaseEntity *pCaller, USE_TYPE useType, float value );
-	void MakerThink ( void );
-	void DeathNotice ( entvars_t *pevChild );// monster maker children use this to tell the monster maker that they have died.
-	void MakeMonster( void );
-
-	DECLARE_DATADESC();
-	
-	string_t	m_iszMonsterClassname;// classname of the monster(s) that will be created.
-	int	m_cNumMonsters;// max number of monsters this ent can create
-	int	m_cLiveChildren;// how many monsters made by this monster maker that are currently alive
-	int	m_iMaxLiveChildren;// max number of monsters that this maker may have out at one time.
-
-	float	m_flGround; // z coord of the ground under me, used to make sure no monsters are under the maker when it drops a new child
-	BOOL	m_fActive;
-	BOOL	m_fFadeChildren;// should we make the children fadeout?
-};
+#include "monstermaker.h"
 
 LINK_ENTITY_TO_CLASS( monstermaker, CMonsterMaker );
 
