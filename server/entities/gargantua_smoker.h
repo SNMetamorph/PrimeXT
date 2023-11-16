c27d6fe9394bf0dd97d17c7381e810c3b6111899
git <APAmk2@users.noreply.github.com>
2023-11-16 21:36:49 +0400

    server: restructurized gargantua source code

---
+++ server/entities/gargantua_smoker.h
@@ -0,0 +1,41 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+
+//=========================================================
+// Gargantua
+//=========================================================
+
+#pragma once
+
+#include	"extdll.h"
+#include	"util.h"
+#include	"cbase.h"
+#include	"nodes.h"
+#include	"monsters.h"
+#include	"schedule.h"
+#include	"customentity.h"
+#include	"weapons.h"
+#include	"effects.h"
+#include	"decals.h"
+#include	"explode.h"
+#include	"func_break.h"
+
+class CSmoker : public CBaseEntity
+{
+	DECLARE_CLASS( CSmoker, CBaseEntity );
+public:
+	void Spawn( void );
+	void Think( void );
+};
---
+++ server/entities/gargantua_spiral.h
@@ -0,0 +1,46 @@
+/***
+*
+*	Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+
+//=========================================================
+// Gargantua
+//=========================================================
+
+#pragma once
+
+#include	"extdll.h"
+#include	"util.h"
+#include	"cbase.h"
+#include	"nodes.h"
+#include	"monsters.h"
+#include	"schedule.h"
+#include	"customentity.h"
+#include	"weapons.h"
+#include	"effects.h"
+#include	"decals.h"
+#include	"explode.h"
+#include	"func_break.h"
+
+#define SPIRAL_INTERVAL		0.1 //025
+
+
+class CSpiral : public CBaseEntity
+{
+	DECLARE_CLASS( CSpiral, CBaseEntity );
+public:
+	void Spawn( void );
+	void Think( void );
+	int ObjectCaps( void ) { return FCAP_DONT_SAVE; }
+	static CSpiral *Create( const Vector &origin, float height, float radius, float duration );
+};
