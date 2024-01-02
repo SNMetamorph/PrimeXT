4f944e1e7eaf3fc83235a790b5cdc78921868872
SNMetamorph <25657591+SNMetamorph@users.noreply.github.com>
2024-01-02 21:00:14 +0400

    utils: pxstudiomdl: changed model activities list for compliance with server-side

--- utils/pxstudiomdl/activity.h
+++ utils/pxstudiomdl/activity.h
@@ -23,21 +23,21 @@ typedef enum
 	ACT_GUARD,
 	ACT_WALK,
 	ACT_RUN,
-	ACT_FLY,			 // Fly (and flap if appropriate)
+	ACT_FLY,				// Fly (and flap if appropriate)
 	ACT_SWIM,
-	ACT_HOP,			 // vertical jump
-	ACT_LEAP,			 // long forward jump
+	ACT_HOP,				// vertical jump
+	ACT_LEAP,				// long forward jump
 	ACT_FALL,
 	ACT_LAND,
 	ACT_STRAFE_LEFT,
 	ACT_STRAFE_RIGHT,
-	ACT_ROLL_LEFT,		 // tuck and roll, left
-	ACT_ROLL_RIGHT,		 // tuck and roll, right
-	ACT_TURN_LEFT,		 // turn quickly left (stationary)
-	ACT_TURN_RIGHT,		 // turn quickly right (stationary)
-	ACT_CROUCH,		 // the act of crouching down from a standing position
-	ACT_CROUCHIDLE,		 // holding body in crouched position (loops)
-	ACT_STAND,		 // the act of standing from a crouched position
+	ACT_ROLL_LEFT,			// tuck and roll, left
+	ACT_ROLL_RIGHT,			// tuck and roll, right
+	ACT_TURN_LEFT,			// turn quickly left (stationary)
+	ACT_TURN_RIGHT,			// turn quickly right (stationary)
+	ACT_CROUCH,				// the act of crouching down from a standing position
+	ACT_CROUCHIDLE,			// holding body in crouched position (loops)
+	ACT_STAND,				// the act of standing from a crouched position
 	ACT_USE,
 	ACT_SIGNAL1,
 	ACT_SIGNAL2,
@@ -51,9 +51,9 @@ typedef enum
 	ACT_MELEE_ATTACK1,
 	ACT_MELEE_ATTACK2,
 	ACT_RELOAD,
-	ACT_ARM,			// pull out gun, for instance
-	ACT_DISARM,		// reholster gun
-	ACT_EAT,			// monster chowing on a large food item (loop)
+	ACT_ARM,				// pull out gun, for instance
+	ACT_DISARM,				// reholster gun
+	ACT_EAT,				// monster chowing on a large food item (loop)
 	ACT_DIESIMPLE,
 	ACT_DIEBACKWARD,
 	ACT_DIEFORWARD,
@@ -65,19 +65,19 @@ typedef enum
 	ACT_SLEEP,
 	ACT_INSPECT_FLOOR,		// for active idles, look at something on or near the floor
 	ACT_INSPECT_WALL,		// for active idles, look at something directly ahead of you ( doesn't HAVE to be a wall or on a wall )
-	ACT_IDLE_ANGRY,		// alternate idle animation in which the monster is clearly agitated. (loop)
-	ACT_WALK_HURT,		// limp  (loop)
-	ACT_RUN_HURT,		// limp  (loop)
-	ACT_HOVER,		// Idle while in flight
-	ACT_GLIDE,		// Fly (don't flap)
-	ACT_FLY_LEFT,		// Turn left in flight
-	ACT_FLY_RIGHT,		// Turn right in flight
+	ACT_IDLE_ANGRY,			// alternate idle animation in which the monster is clearly agitated. (loop)
+	ACT_WALK_HURT,			// limp  (loop)
+	ACT_RUN_HURT,			// limp  (loop)
+	ACT_HOVER,				// Idle while in flight
+	ACT_GLIDE,				// Fly (don't flap)
+	ACT_FLY_LEFT,			// Turn left in flight
+	ACT_FLY_RIGHT,			// Turn right in flight
 	ACT_DETECT_SCENT,		// this means the monster smells a scent carried by the air
 	ACT_SNIFF,				// this is the act of actually sniffing an item in front of the monster
-	ACT_BITE,			// some large monsters can eat small things in one bite. This plays one time, EAT loops.
+	ACT_BITE,				// some large monsters can eat small things in one bite. This plays one time, EAT loops.
 	ACT_THREAT_DISPLAY,		// without attacking, monster demonstrates that it is angry. (Yell, stick out chest, etc )
 	ACT_FEAR_DISPLAY,		// monster just saw something that it is afraid of
-	ACT_EXCITED,		// for some reason, monster is excited. Sees something he really likes to eat, or whatever.
+	ACT_EXCITED,			// for some reason, monster is excited. Sees something he really likes to eat, or whatever.
 	ACT_SPECIAL_ATTACK1,	// very monster specific special attacks.
 	ACT_SPECIAL_ATTACK2,	
 	ACT_COMBAT_IDLE,		// agitated idle.
