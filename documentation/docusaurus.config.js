d6055ebad999a5b4d75881aae2cbe4cd3beb43f3
SNMetamorph <snmetamorph.adc@gmail.com>
2024-05-10 19:23:41 +0400

    documentation: temporary removed blog section from site

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'PrimeXT Docs',
+        title: 'PrimeXT',
         logo: {
           alt: 'PrimeXT Logo',
           src: 'img/logo_256.png',
@@ -74,9 +74,8 @@ const config = {
             type: 'doc',
             docId: 'eng/intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Documentation',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/SNMetamorph/PrimeXT',
             label: 'GitHub',
