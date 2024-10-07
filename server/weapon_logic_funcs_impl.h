1bb7aaeee89950feb440a5bfe89a73c68f9c8e39
SNMetamorph <snmetamorph.adc@gmail.com>
2024-10-07 13:51:58 +0400

    WIP

---
+++ server/weapon_logic_funcs_impl.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "weapon_logic_funcs.h"
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "player.h"
+#include "monsters.h"
+#include "weapons.h"
+
+class CWeaponLogicFuncsImpl : public IWeaponLogicFuncs
+{
+public:
+	CWeaponLogicFuncsImpl(CBasePlayerWeapon *weaponEntity);
+
+	const char *GetWeaponClassname() override;
+	int GetWeaponBodygroup() override;
+
+private:
+	CBasePlayerWeapon *m_pWeapon;
+};
