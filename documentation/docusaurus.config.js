ce04b69b35a07e597ded85e513e756425a723d35
SNMetamorph <snmetamorph.adc@gmail.com>
2024-12-22 03:40:27 +0400

    documentation: changed code Prism highlighting theme for dark mode

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,9 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const {themes} = require('prism-react-renderer');
-const lightTheme = themes.github;
-const darkTheme = themes.dracula;
+import { themes as prismThemes } from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -133,8 +131,8 @@ const config = {
         respectPrefersColorScheme: true,
       },
       prism: {
-        theme: lightTheme,
-        darkTheme: darkTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.oneDark,
       },
       metadata: [
         { name: 'keywords', content: 'hlsdk, gamedev, programming, open source, half-life, xash3d, fwgs, goldsrc, sdk, opengl, physx, xashxt, modding, snmetamorph' }
