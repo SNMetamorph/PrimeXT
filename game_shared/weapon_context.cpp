bdf9d709da1e56f8d5e69ace96a014bb376b75ee
SNMetamorph <snmetamorph.adc@gmail.com>
2025-01-07 22:40:38 +0400

    game_shared: major refactoring of weapon gamelogic code, now it is shared between client and server

---
+++ game_shared/weapon_context.cpp
@@ -0,0 +1,297 @@
+/*
+weapon_context.cpp - part of weapons implementation common for client & server
+Copyright (C) 2024 SNMetamorph
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#include "weapon_context.h"
+#ifdef CLIENT_DLL
+#include "const.h"
+#else
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "player.h"
+#include "monsters.h"
+#include "weapons.h"
+#include "nodes.h"
+#include "soundent.h"
+#include "decals.h"
+#include "gamerules.h"
+#endif
+
+ItemInfo CBaseWeaponContext::ItemInfoArray[ MAX_WEAPONS ];
+AmmoInfo CBaseWeaponContext::AmmoInfoArray[ MAX_AMMO_SLOTS ];
+
+CBaseWeaponContext::CBaseWeaponContext(IWeaponLayer *layer) :
+	m_pLayer(layer),
+	m_fFireOnEmpty(false),
+	m_fInReload(false),
+	m_fInSpecialReload(false),
+	m_flNextPrimaryAttack(0.0f),
+	m_flNextSecondaryAttack(0.0f),
+	m_flPumpTime(0.0f),
+	m_flTimeWeaponIdle(0.0f),
+	m_iClientClip(0),
+	m_iClientWeaponState(0),
+	m_iClip(0),
+	m_iDefaultAmmo(0),
+	m_iPlayEmptySound(false),
+	m_iPrimaryAmmoType(0),
+	m_iSecondaryAmmoType(0)
+{
+}
+
+CBaseWeaponContext::~CBaseWeaponContext()
+{
+	if (m_pLayer) {
+		delete m_pLayer;
+	}
+}
+
+void CBaseWeaponContext::ItemPostFrame()
+{
+	if ((m_fInReload) && m_pLayer->GetPlayerNextAttackTime() <= m_pLayer->GetWeaponTimeBase())
+	{
+		// complete the reload. 
+		int j = Q_min( iMaxClip() - m_iClip, m_pLayer->GetPlayerAmmo(m_iPrimaryAmmoType) );	
+
+		// Add them to the clip
+		m_iClip += j;
+		m_pLayer->SetPlayerAmmo( m_iPrimaryAmmoType,  m_pLayer->GetPlayerAmmo(m_iPrimaryAmmoType) - j );
+
+		m_fInReload = FALSE;
+	}
+
+	if (m_pLayer->CheckPlayerButtonFlag(IN_ATTACK2) && m_flNextSecondaryAttack <= m_pLayer->GetWeaponTimeBase() )
+	{
+		if ( pszAmmo2() && !m_pLayer->GetPlayerAmmo(SecondaryAmmoIndex()) )
+		{
+			m_fFireOnEmpty = TRUE;
+		}
+
+		SecondaryAttack();
+		m_pLayer->ClearPlayerButtonFlag(IN_ATTACK2);
+	}
+	else if (m_pLayer->CheckPlayerButtonFlag(IN_ATTACK) && m_flNextPrimaryAttack <= m_pLayer->GetWeaponTimeBase() )
+	{
+		if ( (m_iClip == 0 && pszAmmo1()) || (iMaxClip() == -1 && !m_pLayer->GetPlayerAmmo(PrimaryAmmoIndex())) )
+		{
+			m_fFireOnEmpty = TRUE;
+		}
+
+		PrimaryAttack();
+	}
+	else if ( m_pLayer->CheckPlayerButtonFlag(IN_RELOAD) && iMaxClip() != WEAPON_NOCLIP && !m_fInReload ) 
+	{
+		// reload when reload is pressed, or if no buttons are down and weapon is empty.
+		Reload();
+	}
+	else if ( !(m_pLayer->CheckPlayerButtonFlag(IN_ATTACK|IN_ATTACK2) ) )
+	{
+		// no fire buttons down
+
+		m_fFireOnEmpty = FALSE;
+#ifndef CLIENT_DLL // we don't need this branch on client side, because client is not responsible for changing weapons
+		if ( !IsUseable() && m_flNextPrimaryAttack < m_pLayer->GetWeaponTimeBase() ) 
+		{
+			// weapon isn't useable, switch. GetNextBestWeapon exactly does weapon switching
+			if ( !(iFlags() & ITEM_FLAG_NOAUTOSWITCHEMPTY) && g_pGameRules->GetNextBestWeapon( m_pLayer->GetWeaponEntity()->m_pPlayer, m_pLayer->GetWeaponEntity() ))
+			{
+				m_flNextPrimaryAttack = m_pLayer->GetWeaponTimeBase() + 0.3;
+				return;
+			}
+		}
+		else
+#endif
+		{
+			// weapon is useable. Reload if empty and weapon has waited as long as it has to after firing
+			if ( m_iClip == 0 && !(iFlags() & ITEM_FLAG_NOAUTORELOAD) && m_flNextPrimaryAttack < m_pLayer->GetWeaponTimeBase() )
+			{
+				Reload();
+				return;
+			}
+		}
+
+		WeaponIdle( );
+		return;
+	}
+	
+	// catch all
+	if ( ShouldWeaponIdle() )
+	{
+		WeaponIdle();
+	}
+}
+
+void CBaseWeaponContext::Holster()
+{ 
+	m_fInReload = FALSE; // cancel any reload in progress.
+	m_pLayer->SetPlayerViewmodel(0);
+#ifndef CLIENT_DLL
+	m_pLayer->GetWeaponEntity()->m_pPlayer->pev->weaponmodel = 0;
+#endif
+}
+
+//=========================================================
+// IsUseable - this function determines whether or not a 
+// weapon is useable by the player in its current state. 
+// (does it have ammo loaded? do I have any ammo for the 
+// weapon?, etc)
+//=========================================================
+bool CBaseWeaponContext :: IsUseable()
+{
+	if ( m_iClip <= 0 )
+	{
+		if ( m_pLayer->GetPlayerAmmo( PrimaryAmmoIndex() ) <= 0 && iMaxAmmo1() != -1 )			
+		{
+			// clip is empty (or nonexistant) and the player has no more ammo of this type. 
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+
+bool CBaseWeaponContext :: CanDeploy()
+{
+	BOOL bHasAmmo = 0;
+
+	if ( !pszAmmo1() )
+	{
+		// this weapon doesn't use ammo, can always deploy.
+		return TRUE;
+	}
+
+	if ( pszAmmo1() )
+	{
+		bHasAmmo |= (m_pLayer->GetPlayerAmmo(m_iPrimaryAmmoType) != 0);
+	}
+	if ( pszAmmo2() )
+	{
+		bHasAmmo |= (m_pLayer->GetPlayerAmmo(m_iSecondaryAmmoType) != 0);
+	}
+	if (m_iClip > 0)
+	{
+		bHasAmmo |= 1;
+	}
+	if (!bHasAmmo)
+	{
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+bool CBaseWeaponContext :: DefaultDeploy( char *szViewModel, char *szWeaponModel, int iAnim, char *szAnimExt, int skiplocal /* = 0 */, int body )
+{
+	if (!CanDeploy( ))
+		return FALSE;
+
+#ifndef CLIENT_DLL
+	//m_pLayer->GetWeaponEntity()->m_pPlayer->TabulateAmmo();
+	m_pLayer->GetWeaponEntity()->m_pPlayer->pev->viewmodel = MAKE_STRING(szViewModel);
+	m_pLayer->GetWeaponEntity()->m_pPlayer->pev->weaponmodel = MAKE_STRING(szWeaponModel);
+	strcpy( m_pLayer->GetWeaponEntity()->m_pPlayer->m_szAnimExtention, szAnimExt );
+#else
+	// gEngfuncs.CL_LoadModel( szViewModel, &m_pPlayer->pev->viewmodel );
+#endif
+	SendWeaponAnim( iAnim, skiplocal, body );
+
+	m_pLayer->SetPlayerNextAttackTime(m_pLayer->GetWeaponTimeBase() + 0.5);
+	m_flTimeWeaponIdle = m_pLayer->GetWeaponTimeBase() + 1.0;
+	return TRUE;
+}
+
+BOOL CBaseWeaponContext :: DefaultReload( int iClipSize, int iAnim, float fDelay, int body )
+{
+	if (m_pLayer->GetPlayerAmmo(m_iPrimaryAmmoType) <= 0)
+		return FALSE;
+
+	int j = Q_min(iClipSize - m_iClip, m_pLayer->GetPlayerAmmo(m_iPrimaryAmmoType));	
+
+	if (j == 0)
+		return FALSE;
+
+	m_pLayer->SetPlayerNextAttackTime(m_pLayer->GetWeaponTimeBase() + fDelay);
+
+	//!!UNDONE -- reload sound goes here !!!
+	SendWeaponAnim( iAnim, UseDecrement() ? 1 : 0, body );
+
+	m_fInReload = TRUE;
+
+	m_flTimeWeaponIdle = m_pLayer->GetWeaponTimeBase() + 3;
+	return TRUE;
+}
+
+void CBaseWeaponContext::SendWeaponAnim( int iAnim, int skiplocal, int body )
+{
+	m_pLayer->SetPlayerWeaponAnim(iAnim);
+
+#ifdef CLIENT_DLL
+	// HUD_SendWeaponAnim( iAnim, body, 0 );
+#else
+	if ( UseDecrement() )
+		skiplocal = 1;
+	else
+		skiplocal = 0;
+
+	if ( skiplocal && ENGINE_CANSKIP( m_pLayer->GetWeaponEntity()->m_pPlayer->edict() ) )
+		return;
+
+	MESSAGE_BEGIN( MSG_ONE, SVC_WEAPONANIM, NULL, m_pLayer->GetWeaponEntity()->m_pPlayer->pev );
+		WRITE_BYTE( iAnim );							// sequence number
+		WRITE_BYTE( m_pLayer->GetWeaponBodygroup() );	// weaponmodel bodygroup.
+	MESSAGE_END();
+#endif
+}
+
+bool CBaseWeaponContext :: PlayEmptySound()
+{
+	if (m_iPlayEmptySound)
+	{
+#ifdef CLIENT_DLL
+		// HUD_PlaySound( "weapons/357_cock1.wav", 0.8 );
+#else
+		EMIT_SOUND(ENT(m_pLayer->GetWeaponEntity()->m_pPlayer->pev), CHAN_WEAPON, "weapons/357_cock1.wav", 0.8, ATTN_NORM);
+#endif
+		m_iPlayEmptySound = 0;
+		return 0;
+	}
+	return 0;
+}
+
+void CBaseWeaponContext :: ResetEmptySound()
+{
+	m_iPlayEmptySound = 1;
+}
+
+int CBaseWeaponContext::PrimaryAmmoIndex()
+{
+	return m_iPrimaryAmmoType;
+}
+
+int CBaseWeaponContext::SecondaryAmmoIndex()
+{
+	return -1;
+}
+
+int CBaseWeaponContext::iItemSlot()				{ return 0; }	// return 0 to MAX_ITEMS_SLOTS, used in hud
+int	CBaseWeaponContext::iItemPosition() 		{ return CBaseWeaponContext::ItemInfoArray[ m_iId ].iPosition; }
+const char *CBaseWeaponContext::pszAmmo1() 		{ return CBaseWeaponContext::ItemInfoArray[ m_iId ].pszAmmo1; }
+int CBaseWeaponContext::iMaxAmmo1() 			{ return CBaseWeaponContext::ItemInfoArray[ m_iId ].iMaxAmmo1; }
+const char *CBaseWeaponContext::pszAmmo2() 		{ return CBaseWeaponContext::ItemInfoArray[ m_iId ].pszAmmo2; }
+int	CBaseWeaponContext::iMaxAmmo2() 			{ return CBaseWeaponContext::ItemInfoArray[ m_iId ].iMaxAmmo2; }
+const char *CBaseWeaponContext::pszName() 		{ return CBaseWeaponContext::ItemInfoArray[ m_iId ].pszName; }
+int	CBaseWeaponContext::iMaxClip() 				{ return CBaseWeaponContext::ItemInfoArray[ m_iId ].iMaxClip; }
+int	CBaseWeaponContext::iWeight() 				{ return CBaseWeaponContext::ItemInfoArray[ m_iId ].iWeight; }
+int CBaseWeaponContext::iFlags() 				{ return CBaseWeaponContext::ItemInfoArray[ m_iId ].iFlags; }
---
+++ game_shared/weapon_layer.h
@@ -0,0 +1,109 @@
+/*
+weapon_layer.h - interface for abstracting client & server weapons implementation differences
+Copyright (C) 2024 SNMetamorph
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+#pragma once
+#include "vector.h"
+#include "matrix.h"
+#include <stdint.h>
+
+#define _CLASSNAME_STR(s) (#s)
+#define CLASSNAME_STR(s) _CLASSNAME_STR(s)
+
+#define AUTOAIM_2DEGREES  0.0348994967025
+#define AUTOAIM_5DEGREES  0.08715574274766
+#define AUTOAIM_8DEGREES  0.1391731009601
+#define AUTOAIM_10DEGREES 0.1736481776669
+
+// do not change, these flags are shared with engine
+enum class WeaponEventFlags : int
+{
+	// Empty flag
+	None = 0,
+
+	// Skip local host for event send.
+	NotHost	= (1<<0), 
+
+	// Send the event reliably. You must specify the origin and angles and use
+	// PLAYBACK_EVENT_FULL for this to work correctly on the server for anything
+	// that depends on the event origin/angles.  I.e., the origin/angles are not
+	// taken from the invoking edict for reliable events.
+	Reliable = (1<<1),	 
+
+	// Don't restrict to PAS/PVS, send this event to _everybody_ on the server ( useful for stopping CHAN_STATIC
+	// sounds started by client event when client is not in PVS anymore ( hwguy in TFC e.g. ).
+	Global = (1<<2),
+
+	// If this client already has one of these events in its queue, just update the event instead of sending it as a duplicate
+	Update = (1<<3),
+
+	// Only send to entity specified as the invoker
+	HostOnly = (1<<4),
+
+	// Only send if the event was created on the server.
+	Server = (1<<5),
+
+	// Only issue event client side ( from shared code )
+	Client = (1<<6)
+};
+
+struct WeaponEventParams
+{
+	WeaponEventFlags flags;
+	float *origin;
+	float *angles;
+	float delay;
+	float fparam1;
+	float fparam2; 
+	int iparam1;
+	int iparam2; 
+	int bparam1; 
+	int bparam2;
+	uint16_t eventindex;
+};
+
+// forward declaration, client should not know anything about server entities
+class CBasePlayerWeapon;
+
+class IWeaponLayer
+{
+public:
+	virtual ~IWeaponLayer() {};
+
+	// accessing weapon entity state
+	virtual int GetWeaponBodygroup() = 0;
+	virtual Vector GetGunPosition() = 0;
+	virtual matrix3x3 GetCameraOrientation() = 0;
+	virtual Vector GetAutoaimVector(float delta) = 0;
+	virtual Vector FireBullets(int bullets, Vector origin, matrix3x3 orientation, float distance, float spread, int bulletType, uint32_t seed, int damage = 0) = 0;
+	virtual CBasePlayerWeapon* GetWeaponEntity() = 0;
+
+	// modifying/accessing player state
+	virtual int GetPlayerAmmo(int ammoType) = 0;
+	virtual void SetPlayerAmmo(int ammoType, int count) = 0;
+	virtual void SetPlayerWeaponAnim(int anim) = 0;
+	virtual void SetPlayerViewmodel(int model) = 0;
+	virtual bool CheckPlayerButtonFlag(int buttonMask) = 0;
+	virtual void ClearPlayerButtonFlag(int buttonMask) = 0;
+	virtual float GetPlayerNextAttackTime() = 0;
+	virtual void SetPlayerNextAttackTime(float value) = 0;
+
+	// miscellaneous things
+	virtual float GetWeaponTimeBase() = 0;
+	virtual uint32_t GetRandomSeed() = 0;
+	virtual uint32_t GetRandomInt(uint32_t seed, int32_t min, int32_t max) = 0;
+	virtual float GetRandomFloat(uint32_t seed, float min, float max) = 0;
+	virtual uint16_t PrecacheEvent(const char *eventName) = 0;
+	virtual void PlaybackWeaponEvent(const WeaponEventParams &params) = 0;
+};
