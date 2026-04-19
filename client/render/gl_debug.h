db3af33eda1f9721abd5372b05ea60330c7cdb90
Steven Linn <smlucf@gmail.com>
2026-04-19 13:39:54 -0600

    client: migrate gl_debug.cpp debug drawing to CDebugVisualizer

--- client/render/gl_debug.h
+++ client/render/gl_debug.h
@@ -12,12 +12,10 @@
 void GL_CheckForErrorsInternal(const char *filename, int line);
 void DBG_PrintVertexVBOSizes();
 void DBG_DrawLightFrustum();
-void DBG_DrawFrustum(const CFrustum &frustum);
 void DBG_DrawGlassScissors();
 void DrawLightProbes();
 void R_ShowLightMaps();
 void R_RenderLightProbeInternal(const Vector &origin, const Vector lightCube[]);
-void DBG_DrawBBox(const Vector &mins, const Vector &maxs);
 void DrawWirePoly(msurface_t *surf);
 void DrawTangentSpaces();
 void DrawWireFrame();
